293e7ee21b1ec0c69ffb231697f738c8ec2612d5
Markki Meyer <markkimeyer@gmail.com>
2021-03-12 07:31:48 -0600

    added schema

---
+++ db/schema.sql
@@ -0,0 +1,32 @@
+DROP DATABASE if exists company_db;
+CREATE DATABASE company_db;
+
+USE company_db;
+
+CREATE TABLE department (
+    depid INT AUTO_INCREMENT NOT NULL,
+    dept_name VARCHAR(30) NOT NULL
+    PRIMARY KEY(depid)
+);
+
+CREATE TABLE role(
+    roleid INT AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DEC (10,2),
+    dept_id INT NOT NULL,
+    PRIMARY KEY (roleid),
+    FOREIGN KEY (dept_id) REFERENCES department(depid)
+);
+
+CREATE TABLE employee(
+    empid INT AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    PRIMARY KEY (empid),
+    FOREIGN KEY (role_id) REFERENCES role(roleid),
+    FOREIGN KEY (manager_id) REFERENCES employee(empid)
+);
+
+
